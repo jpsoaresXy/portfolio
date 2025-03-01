9b0eec61e78618d6c5f47a77cdcdaca6a4ab547c
jpsoaresXy <jpsoares3@gmail.com>
2025-03-01 19:30:51 -0300

    initial setup

---
+++ components/Preloader.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { useState } from "react";
+import { MultiStepLoader as Loader } from "./ui/multi-step-loader";
+import { IconSquareRoundedX } from "@tabler/icons-react";
+
+const loadingStates = [
+  {
+    text: "Buying a condo",
+  },
+  {
+    text: "Travelling in a flight",
+  },
+  {
+    text: "Meeting Tyler Durden",
+  },
+  {
+    text: "He makes soap",
+  },
+  {
+    text: "We goto a bar",
+  },
+  {
+    text: "Start a fight",
+  },
+  {
+    text: "We like it",
+  },
+  {
+    text: "Welcome to F**** C***",
+  },
+];
+
+export function Preloader() {
+  const [loading, setLoading] = useState(false);
+  return (
+    <div className="w-full h-[60vh] flex items-center justify-center">
+      {/* Core Loader Modal */}
+      <Loader loadingStates={loadingStates} loading={loading} duration={2000} />
+
+    </div>
+  );
+}
