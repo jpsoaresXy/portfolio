7b0f4126660672be8522dcf3f977b69d89d0a732
jpsoaresXy <jpsoares3@gmail.com>
2025-03-03 19:14:25 -0300

    fix: image optimization

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -12,6 +12,7 @@ import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
+import Image from "next/image";
 
 export const BentoGrid = ({
   className,
@@ -88,9 +89,12 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
-            <img
+            <Image
               src={img}
               alt={img}
+              width={36}
+              height={36}
+              quality={30}
               className={cn(imgClassName, "object-cover object-center ")}
             />
           )}
@@ -101,10 +105,11 @@ export const BentoGridItem = ({
           } `}
         >
           {spareImg && (
-            <img
+            <Image
               src={spareImg}
               alt={spareImg}
-              //   width={220}
+              width={36}
+              height={36}
               className="object-cover object-center w-full h-full"
             />
           )}
@@ -166,9 +171,7 @@ export const BentoGridItem = ({
                 className={`absolute -bottom-5 right-0 ${
                   copied ? "block" : "block"
                 }`}
-              >
-                <Lottie options={defaultOptions} height={200} width={400} />
-              </div>
+              ></div>
 
               <MagicButton
                 title={copied ? "Email is Copied!" : "Copy my email address"}
