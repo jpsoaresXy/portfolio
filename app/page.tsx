4f81133d972642bbd8be52c50a01c095df5208c9
jpsoaresXy <jpsoares3@gmail.com>
2025-03-03 19:29:09 -0300

    fix: disable preload

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ const Home = () => {
 
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      {isLoading && <Preloader isLoading={isLoading} />}
+      {/*isLoading && <Preloader isLoading={isLoading} /> */}
 
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
